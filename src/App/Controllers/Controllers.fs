9c2d02cc71da5826e32e97ef0fcc28a05200ecfa
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-11-15 20:26:51 +0100

    upcast

--- src/App/Controllers/Controllers.fs
+++ src/App/Controllers/Controllers.fs
@@ -5,24 +5,23 @@ open CQRSLite.Core.Domain.ReadModel
 open CQRSLite.Core.Domain.WriteModel
 open FSharpPlus
 open Microsoft.AspNetCore.Mvc
-type AR = IActionResult
 
 type ControllerBase with
     /// interpret query result as Http response (i.e. IActionResult)
-    member self.InterpretQueryResult<'TQueryResult> (qr:Result<'TQueryResult,unit>) = match qr with | Ok v-> self.Ok v :> AR | Error () -> self.NotFound() :> AR
+    member self.InterpretQueryResult<'TQueryResult> (qr:Result<'TQueryResult,unit>) : IActionResult = match qr with | Ok v-> upcast ( self.Ok v ) | Error () -> upcast ( self.NotFound() )
     /// we want to bind the query result into an interpretation of that result  (.i.e Async<IActionResult>) and change Async to Task
     member self.BindReturnQueryInterpretationToTask value = value >>= (self.InterpretQueryResult >> async.Return) |> Async.StartAsTask // note the ceremony
     /// interpret command result as Http response (i.e. IActionResult)
     member self.InterpretCommandResult<'T> (v:Result<unit,ErrorT>) : IActionResult=
       match v with
-      | Ok ()-> self.Ok () :> AR
+      | Ok ()-> upcast (self.Ok ())
       | Error err ->
         match err with
-        | MissingItem -> self.NotFound() :> AR
-        | MissingName -> self.BadRequest("Missing name") :> AR
-        | CantRemoveNegativeCountFromInventory -> self.BadRequest("Cant remove negative count from") :> AR
+        | MissingItem -> upcast (self.NotFound())
+        | MissingName -> upcast (self.BadRequest("Missing name"))
+        | CantRemoveNegativeCountFromInventory -> upcast (self.BadRequest("Cant remove negative count from"))
         | MustHaveACountGreaterThan0ToAddToInventory ->
-          self.BadRequest("Must have a count greater than 0 to add to inventory") :> AR
-        | AlreadyDeactivated -> self.BadRequest("Missing name") :> AR
+          upcast (self.BadRequest("Must have a count greater than 0 to add to inventory"))
+        | AlreadyDeactivated -> upcast (self.BadRequest("Missing name"))
     /// we want to bind the command result into an interpretation of that result (.i.e Async<IActionResult>) and change Async to Task
     member self.BindReturnCommandResult value= value >>= (self.InterpretCommandResult >> async.Return) |> Async.StartAsTask // note the ceremony         
